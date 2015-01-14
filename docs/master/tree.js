0f9a36ebcad16c9b8579ae65af42db7bc363f5be
Ben Batschelet <ben@codeup.com>
2015-01-14 11:36:53 -0600

    Actual documentation, not just page structure. Duh.

--- docs/master/tree.js
+++ docs/master/tree.js
@@ -1,2 +1,11 @@
-var tree = [        ]
+var tree = [["Bbatsche",,"",
+                [["Entrust","Bbatsche\/Entrust.html","",
+                [["Entrust","Bbatsche\/Entrust\/Entrust.html","",
+                [        ]],["EntrustFacade","Bbatsche\/Entrust\/EntrustFacade.html"," < Facade",
+                [        ]],["EntrustPermission","Bbatsche\/Entrust\/EntrustPermission.html"," < Model",
+                [        ]],["EntrustRole","Bbatsche\/Entrust\/EntrustRole.html"," < Model",
+                [        ]],["EntrustServiceProvider","Bbatsche\/Entrust\/EntrustServiceProvider.html"," < ServiceProvider",
+                [        ]],["HasRole","Bbatsche\/Entrust\/HasRole.html","",
+                [        ]],["MigrationCommand","Bbatsche\/Entrust\/MigrationCommand.html"," < Command",
+                [        ]]        ]]        ]]        ]
 
